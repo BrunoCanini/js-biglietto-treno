724efc45e61dfa6ec0a384fcd7b09172ada2bbdc
Bruno Canini <brunocanini1994@gmail.com>
2023-04-13 18:49:49 +0200

    inserimento allert, html

--- main.js
+++ main.js
@@ -7,11 +7,13 @@ alert("Prezzo biglietto 0.21$ al Km, 20% sconto per minorenni, 40% sconto per ov
 
 const kmUtente = prompt("Km che si vogliono percorrere?");
 const prezzoKm = ((kmUtente)*0.21);
+document.getElementById("kmCorsa").innerText = kmUtente + "Km";
 console.log(prezzoKm);
 
 let sconto = 0
 
 const etaUtente = prompt("Si prega di inserire l'età");
+document.getElementById("etaPasseggero").innerText = etaUtente;
 
 if (etaUtente < 18) {
     sconto = 20;
@@ -21,6 +23,8 @@ if (etaUtente < 18) {
 
 let prezzoBiglietto = prezzoKm - (prezzoKm * sconto /100);
 prezzoBiglietto = Math.floor(prezzoBiglietto * 100) / 100;
-
+// let prezzoBigliettoFinale = prezzoBiglietto.toFixed(2);
+alert(prezzoBiglietto + "Euro")
+document.getElementById("prezzo").innerText = prezzoBiglietto + "Euro";
 
 console.log(prezzoBiglietto, sconto)
